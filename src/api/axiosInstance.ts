a7e70768a7b2a7463d10331bf14eaa82eee23746
Irfan <irfangunaw31@gmail.com>
2025-04-29 21:12:59 +0700

    update

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -7,7 +7,7 @@ const getCookie = (name: string): string | null => {
   }
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API || "http://localhost:8000",
+  baseURL: process.env.NEXT_PUBLIC_API,
   // withCredentials: true,
   headers: {
     "Content-Type": "application/json",
